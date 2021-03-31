4dd0864d351813020eb43a259515871e994aac1c
Amoghtech <you@example.com>
2021-03-31 13:43:08 +0530

    "New problems "

---
+++ A. Panoramix&apos;s Prediction.cpp
@@ -0,0 +1,90 @@
+#define nbits(n) __builtin_popcount(n)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+#define mod 1000000007
+#define up(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+#define lp(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+#define maxv(a) max_element(a.begin(), a.end()) - a.begin();
+#define minv(a) min_element(a.begin(), a.end()) - a.begin();
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define pairi pair<ll, ll>
+#define vi vector<ll>
+#define mapi map<ll, ll>
+#define mapci map<char, ll>
+#define mapsi map<string, ll>
+#define pq priority_queue<ll>
+#define pqr priority_queue<ll, vi, greater<ll>>
+#define minele(a, n) *min_element(a, a + n);
+/*******************************/
+#define w(x)  \
+    ll x;     \
+    cin >> x; \
+    while (x--)
+#define take(n) \
+    ll n;       \
+    cin >> n
+#define takearray(a, n)        \
+    ll a[n];                   \
+    for (ll i = 0; i < n; i++) \
+        cin >> a[i];
+#define takeab(a, b) \
+    ll a, b;         \
+    cin >> a >> b;
+#define ctoupper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+#define ctolower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+bool isUpper(string s)
+{
+    return std::all_of(s.begin(), s.end(), [](unsigned char c) { return std::isupper(c); });
+}
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int i = n + 1;
+    bool is = false;
+    while (!is)
+    {
+        if (isPrime(i))
+        {
+            is = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (i == m)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
---
+++ A. Translation.cpp
@@ -0,0 +1,74 @@
+#define nbits(n) __builtin_popcount(n)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+#define mod 1000000007
+#define up(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+#define lp(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+#define maxv(a) max_element(a.begin(), a.end()) - a.begin();
+#define minv(a) min_element(a.begin(), a.end()) - a.begin();
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define pairi pair<ll, ll>
+#define vi vector<ll>
+#define mapi map<ll, ll>
+#define mapci map<char, ll>
+#define mapsi map<string, ll>
+#define pq priority_queue<ll>
+#define pqr priority_queue<ll, vi, greater<ll>>
+#define minele(a, n) *min_element(a, a + n);
+/*******************************/
+#define w(x)  \
+    ll x;     \
+    cin >> x; \
+    while (x--)
+#define take(n) \
+    ll n;       \
+    cin >> n
+#define takearray(a, n)        \
+    ll a[n];                   \
+    for (ll i = 0; i < n; i++) \
+        cin >> a[i];
+#define takeab(a, b) \
+    ll a, b;         \
+    cin >> a >> b;
+#define ctoupper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+#define ctolower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+bool isUpper(string s)
+{
+    return std::all_of(s.begin(), s.end(), [](unsigned char c) { return std::isupper(c); });
+}
+// 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210, 231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496
+string removeSpaces(string str)
+{
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+#define reverse(x) reverse(x.begin(), x.end())
+int main()
+{
+       return 0;
+}
